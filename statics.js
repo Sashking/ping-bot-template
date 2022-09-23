79c4f1d14496bb1cfa0ca18383d03771250f80e9
regex <64776862+Sashking@users.noreply.github.com>
2022-09-23 08:48:52 +0200

    Create statics.js

---
+++ statics.js
@@ -0,0 +1,6 @@
+const timeout = 100
+const pingRole = "<@&YOUR_PING_ROLE_ID_HERE>"
+const categories = ['1234567890', '1234567890', '1234567890', 'add as many categories as you want', '...']
+const serverId = "YOUR_PING_SERVER_ID_HERE"
+
+module.exports = { timeout, pingRole, categories, serverId }
